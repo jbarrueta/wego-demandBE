daf6cbaca0dc60195de05fc991b51e41c42c529b
Jorge Barrueta <jbarrueta99@hotmail.com>
2021-04-25 18:58:02 -0500

    JB: fix

--- classes/customer.py
+++ classes/customer.py
@@ -1,37 +1,44 @@
 import re
 # Customer class
+
+
 class Customer:
-    def __init__(self, email, first_name=None, last_name=None, password=None):
+    def __init__(self, email=None, first_name=None, last_name=None, password=None):
         if not(first_name == None or last_name == None):
             self.setFirstName(first_name)
             self.setLastName(last_name)
-        self.setEmail(email)
-        self.setPassword(password)
-    
+        if email != None:
+            self.setEmail(email)
+        if password != None:
+            self.setPassword(password)
+
     def setFirstName(self, first_name):
         if isinstance(first_name, str):
             self.first_name = first_name
         else:
-            raise ValueError("First name must be a string and at least 2 characters")
+            raise ValueError(
+                "First name must be a string and at least 2 characters")
 
     def setLastName(self, last_name):
         if isinstance(last_name, str):
             self.last_name = last_name
         else:
-            raise ValueError("Last name must be a string and at least 2 characters")
-    
+            raise ValueError(
+                "Last name must be a string and at least 2 characters")
+
     def setEmail(self, email):
         if re.match("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$", email):
             self.email = email
-        else: 
+        else:
             raise ValueError("Invalid email passed!")
-    
+
     def setPassword(self, password):
         if(re.match("^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$", password)):
             self.password = password
         else:
-            raise ValueError("Passwords must be 6 to 20 characters which contain at least one numeric digit, one uppercase and one lowercase letter")
-    
+            raise ValueError(
+                "Passwords must be 6 to 20 characters which contain at least one numeric digit, one uppercase and one lowercase letter")
+
     def getFirstName(self):
         return self.first_name
 
@@ -43,8 +50,6 @@ class Customer:
 
     def get_register_data(self):
         return self.__dict__
-    
+
     def get_login_data(self):
         return self.email, self.password
-
-
--- test/customerUnitTest.py
+++ test/customerUnitTest.py
@@ -1,9 +1,8 @@
 import sys
 sys.path.append("..")
-# Unit test for Customer Class
 from classes.customer import Customer
 import unittest
-
+# Unit test for Customer Class
 
 
 validEmail1 = "johndoe@email.com"
@@ -24,40 +23,52 @@ invalidLastName2 = 1299
 invalidPassword1 = "password"
 invalidPassword2 = "test1234"
 
+
 class TestCustomer(unittest.TestCase):
 
     def test_valid_user_1(self):
-        validUser1 = Customer(validEmail1, validFirstName1, validLastName1, validPassword1)
+        validUser1 = Customer(validEmail1, validFirstName1,
+                              validLastName1, validPassword1)
         registerObj = validUser1.get_register_data()
-        match = {'email': validEmail1, 'first_name': validFirstName1, "last_name": validLastName1, "password": validPassword1}
-        self.assertEqual(registerObj, match, msg=f"registerObj does not match\n\nregisterObj: {registerObj}\n\ncorrect: {match}")
-    
+        match = {'email': validEmail1, 'first_name': validFirstName1,
+                 "last_name": validLastName1, "password": validPassword1}
+        self.assertEqual(
+            registerObj, match, msg=f"registerObj does not match\n\nregisterObj: {registerObj}\n\ncorrect: {match}")
+
     def test_valid_user_2(self):
-        validUser2 = Customer(validEmail2, validFirstName2, validLastName2, validPassword2)
+        validUser2 = Customer(validEmail2, validFirstName2,
+                              validLastName2, validPassword2)
         registerObj = validUser2.get_register_data()
-        match = {'email': validEmail2, 'first_name': validFirstName2, "last_name": validLastName2, "password": validPassword2}
-        self.assertEqual(registerObj, match, msg=f"registerObj does not match\n\nregisterObj: {registerObj}\n\ncorrect: {match}")
-    
+        match = {'email': validEmail2, 'first_name': validFirstName2,
+                 "last_name": validLastName2, "password": validPassword2}
+        self.assertEqual(
+            registerObj, match, msg=f"registerObj does not match\n\nregisterObj: {registerObj}\n\ncorrect: {match}")
+
     def test_invalid_user_1(self):
         with self.assertRaises(ValueError, msg="Values passed should have raised a Value Error"):
-            Customer(invalidEmail1, invalidFirstName1, invalidLastName1, invalidPassword1)            
-    
+            Customer(invalidEmail1, invalidFirstName1,
+                     invalidLastName1, invalidPassword1)
+
     def test_invalid_user_2(self):
         with self.assertRaises(ValueError, msg="Values passed should have raised a Value Error"):
-            Customer(invalidEmail2, invalidFirstName2, invalidLastName2, invalidPassword2)
+            Customer(invalidEmail2, invalidFirstName2,
+                     invalidLastName2, invalidPassword2)
+
 
 class TestCustomerSetters(TestCustomer):
     def test_email_setter_1(self):
         user = Customer()
         user.setEmail(validEmail1)
         correctValue = validEmail1
-        self.assertEqual(Customer.getEmail(), correctValue, msg=f"Error in the email setter and getter values:\n\n{Customer.getEmail()}\n\n{correctValue}")
+        self.assertEqual(user.getEmail(), correctValue,
+                         msg=f"Error in the email setter and getter values:\n\n{user.getEmail()}\n\n{correctValue}")
 
     def test_invalid_email_setter_1(self):
         with self.assertRaises(ValueError, msg="Values passed should have raised a Value Error"):
-                user = Customer() 
-                user.setEmail(invalidEmail1)
-    
+            user = Customer()
+            user.setEmail(invalidEmail1)
+
+
 if __name__ == '__main__':
     unittest.main()
 
@@ -116,5 +127,3 @@ if __name__ == '__main__':
 #     def test_get_login_data(self):
 #         self.assertEqual(self.customer.get_login_data(),
 #                          ("test@test.com", "testpass"))
-
-
